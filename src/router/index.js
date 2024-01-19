55e7610ed405af48e2f650846a723265e239af58
Lenildo-Luan <lenildoluan@gmail.com>
2024-01-19 09:50:50 -0300

    add not found page and route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 
 import PageHome from '@/components/PageHome'
 import PageThreadShow from '@/components/PageThreadShow'
+import PageNotFound from '@/components/PageNotFound'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -14,6 +15,11 @@ const routes = [
     name: 'ThreadShow',
     component: PageThreadShow,
     props: true
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: PageNotFound
   }
 ]
 
