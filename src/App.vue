a4f2df022546662131da286cad86a052947fd28f
Lenildo-Luan <lenildoluan@gmail.com>
2024-01-19 09:33:46 -0300

    Dynamic Route Matching & Route Params

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,13 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
   <div class="container">
-    <PageHome/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import PageHome from './components/PageHome.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    PageHome
-  }
+  name: 'App'
 }
 </script>
 
