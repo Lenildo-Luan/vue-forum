29510550e5ddb90b811483856ee82d249dc5cd0e
Lenildo-Luan <lenildoluan@gmail.com>
2024-02-02 19:07:55 -0300

    using authId for new posts and clean up

--- src/router/index.js
+++ src/router/index.js
@@ -1,80 +1,82 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import Home from '@/pages/Home'
-import ThreadShow from '@/pages/ThreadShow'
-import NotFound from '@/pages/NotFound'
-import Forum from '@/pages/Forum'
-import Category from '@/pages/Category'
-import Profile from '@/pages/Profile'
+import Home from "@/pages/Home";
+import ThreadShow from "@/pages/ThreadShow";
+import NotFound from "@/pages/NotFound";
+import Forum from "@/pages/Forum";
+import Category from "@/pages/Category";
+import Profile from "@/pages/Profile";
 
-import sourceData from '@/data.json'
+import sourceData from "@/data.json";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/thread/:id',
-    name: 'ThreadShow',
+    path: "/thread/:id",
+    name: "ThreadShow",
     component: ThreadShow,
     props: true,
-    beforeEnter (to, from, next) {
+    beforeEnter(to, from, next) {
       // check if thread exists
-      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
+      const threadExists = sourceData.threads.find(
+        (thread) => thread.id === to.params.id
+      );
       // if exists continue
       if (threadExists) {
-        return next()
+        return next();
       } else {
         next({
-          name: 'NotFound',
-          params: { pathMatch: to.path.substring(1).split('/') },
+          name: "NotFound",
+          params: { pathMatch: to.path.substring(1).split("/") },
           // preserve existing query and hash
           query: to.query,
-          hash: to.hash
-        })
+          hash: to.hash,
+        });
       }
       // if doesnt exist redirect to not found
-    }
+    },
   },
   {
-    path: '/:pathMatch(.*)*',
-    name: 'NotFound',
-    component: NotFound
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: NotFound,
   },
   {
-    path: '/forum/:id',
-    name: 'Forum',
+    path: "/forum/:id",
+    name: "Forum",
     component: Forum,
-    props: true
+    props: true,
   },
   {
-    path: '/category/:id',
-    name: 'Category',
+    path: "/category/:id",
+    name: "Category",
     component: Category,
-    props: true
+    props: true,
   },
   {
-    path: '/me',
-    name: 'Profile',
-    component: Profile
-  },
-  {
-    path: '/me/edit',
-    name: 'ProfileEdit',
+    path: "/me",
+    name: "Profile",
     component: Profile,
-    meta: { toTop: true, smoothScroll: true }
   },
-]
+  {
+    path: "/me/edit",
+    name: "ProfileEdit",
+    component: Profile,
+    meta: { toTop: true, smoothScroll: true },
+  },
+];
 
 export default createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior (to) {
-    const scroll = {}
-    if (to.meta.toTop) scroll.top = 0
-    if (to.meta.smoothScroll) scroll.behavior = 'smooth'
-    return scroll
-  }
-})
+  scrollBehavior(to) {
+    const scroll = {};
+    if (to.meta.toTop) scroll.top = 0;
+    if (to.meta.smoothScroll) scroll.behavior = "smooth";
+    return scroll;
+  },
+});
--- src/store/index.js
+++ src/store/index.js
@@ -7,38 +7,40 @@ export default createStore({
     authId: "VXjpr2WHa8Ux4Bnggym8QFLdv5C3",
   },
   getters: {
-    authUser: state => {
-      const user = state.users.find(user => user.id === state.authId)
-      if (!user) return null
-      return { 
+    authUser: (state) => {
+      const user = state.users.find((user) => user.id === state.authId);
+      if (!user) return null;
+      return {
         ...user,
-        get posts () {
-          return state.posts.filter(post => post.userId === user.id)
+        get posts() {
+          return state.posts.filter((post) => post.userId === user.id);
         },
-        get postsCount () {
-          return this.posts.length
+        get postsCount() {
+          return this.posts.length;
         },
-        get threads () {
-          return state.threads.filter(post => post.userId === user.id)
+        get threads() {
+          return state.threads.filter((post) => post.userId === user.id);
         },
-        get threadsCount () {
-          return this.threads.length
-        }
-      }
-    }
+        get threadsCount() {
+          return this.threads.length;
+        },
+      };
+    },
   },
   actions: {
-    createPost(context, post) {
+    createPost({ commit, state }, post) {
       post.id = "ggqq" + Math.random();
-      context.commit("setPost", { post }); // set the post
-      context.commit("appendPostToThread", {
+      post.userId = state.authId;
+      post.publishedAt = Math.floor(Date.now() / 1000);
+      commit("setPost", { post }); // set the post
+      commit("appendPostToThread", {
         postId: post.id,
         threadId: post.threadId,
       }); // append post to thread
     },
-    updateUser ({ commit }, user) {
-      commit('setUser', { user, userId: user.id })
-    }
+    updateUser({ commit }, user) {
+      commit("setUser", { user, userId: user.id });
+    },
   },
   mutations: {
     setPost(state, { post }) {
@@ -48,9 +50,9 @@ export default createStore({
       const thread = state.threads.find((thread) => thread.id === threadId);
       thread.posts.push(postId);
     },
-    setUser (state, { user, userId }) {
-      const userIndex = state.users.findIndex(user => user.id === userId)
-      state.users[userIndex] = user
+    setUser(state, { user, userId }) {
+      const userIndex = state.users.findIndex((user) => user.id === userId);
+      state.users[userIndex] = user;
     },
   },
 });
